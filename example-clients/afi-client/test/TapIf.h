a24a1a7df675ae667694a53bac78068efb4a9cb1
Renato Westphal <renato@opensourcerouting.org>
2017-06-30 22:48:43 -0300

    example-clients: convert tabs to spaces
    We should use either tabs or spaces for indentation, not both. Since
    the majority of the existing code uses spaces, get rid of the tabs
    for consistency.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- example-clients/afi-client/test/TapIf.h
+++ example-clients/afi-client/test/TapIf.h
@@ -28,32 +28,32 @@ class TapIf
 {
 public:
     TapIf (const std::string &ifNameStr) {
-	    strncpy(_ifName, ifNameStr.c_str(), IFNAMSIZ);
-		_ifName[IFNAMSIZ - 1] = '\0';
-	}
+        strncpy(_ifName, ifNameStr.c_str(), IFNAMSIZ);
+        _ifName[IFNAMSIZ - 1] = '\0';
+    }
 
-	~TapIf () {
-	}
+    ~TapIf () {
+    }
 
-	int init(void) {
-		int tapFd;
-		int flags = IFF_TAP;
+    int init(void) {
+        int tapFd;
+        int flags = IFF_TAP;
 
-		/* initialize tun/tap interface */
-		if ( (tapFd = tapAlloc(_ifName, flags | IFF_NO_PI)) < 0 ) {
-			std::cout << "Error connecting to tap interface " << _ifName << std::endl;
-			return -1;
-		}
-		_tapFd = tapFd;
-		return _tapFd;
-	}
+        /* initialize tun/tap interface */
+        if ( (tapFd = tapAlloc(_ifName, flags | IFF_NO_PI)) < 0 ) {
+            std::cout << "Error connecting to tap interface " << _ifName << std::endl;
+            return -1;
+        }
+        _tapFd = tapFd;
+        return _tapFd;
+    }
 
     int tapAlloc(char *dev, int flags);
-	int ifRead(void);
+    int ifRead(void);
 
 private:
     char _ifName[IFNAMSIZ];
-	int  _tapFd;
+    int  _tapFd;
 };
 
 #endif // __TapIf__
--- example-clients/afi-client/test/TestPacket.h
+++ example-clients/afi-client/test/TestPacket.h
@@ -29,7 +29,7 @@
 #include "../Utils.h"
 
 #define MAC_ADDR_LEN            6
-#define ETHER_PAYLOAD_BUF_SIZE	5000
+#define ETHER_PAYLOAD_BUF_SIZE  5000
 
 class TestPacket {
 public:
@@ -37,96 +37,96 @@ public:
     //
     //
     //
-	int setSrcMac(const std::string &mac) {
-		int i;
+    int setSrcMac(const std::string &mac) {
+        int i;
         std::vector<std::string> mac_sub_strings;
         boost::split(mac_sub_strings, mac, boost::is_any_of(":"));
 
-		for (i = 0; i < MAC_ADDR_LEN; i++) {
+        for (i = 0; i < MAC_ADDR_LEN; i++) {
             _srcMac[i] = std::strtoull(mac_sub_strings.at(i).c_str(), NULL, 16);
         }
     }
 
-	int *getSrcMac(void) {
-	    return _srcMac;
-	}
+    int *getSrcMac(void) {
+        return _srcMac;
+    }
 
     //
     //
     //
-	int setDstMac(const std::string &mac) {
-		int i;
+    int setDstMac(const std::string &mac) {
+        int i;
         std::vector<std::string> mac_sub_strings;
         boost::split(mac_sub_strings, mac, boost::is_any_of(":"));
 
-		for (i = 0; i < MAC_ADDR_LEN; i++) {
+        for (i = 0; i < MAC_ADDR_LEN; i++) {
             _dstMac[i] = std::strtoull(mac_sub_strings.at(i).c_str(), NULL, 16);
         }
     }
 
-	int *getDstMac(void) {
-	    return _dstMac;
-	}
+    int *getDstMac(void) {
+        return _dstMac;
+    }
 
     //
-	//
-	//
-	int getEtherType(void) {
+    //
+    //
+    int getEtherType(void) {
         return _etherType;
     }
 
-	void setEtherType(int  etherType) {
+    void setEtherType(int  etherType) {
         _etherType = etherType;
     }
 
     //
-	//
-	//
-	int setEtherPayloadStr(const std::string &etherPayloadStr) {
+    //
+    //
+    int setEtherPayloadStr(const std::string &etherPayloadStr) {
         strncpy(_etherPayload, etherPayloadStr.c_str(), ETHER_PAYLOAD_BUF_SIZE);
-		_etherPayload[ETHER_PAYLOAD_BUF_SIZE - 1] = '\0';
+        _etherPayload[ETHER_PAYLOAD_BUF_SIZE - 1] = '\0';
     }
 
-	char *getEtherPayloadStr(void) {
-	      return _etherPayload;
-	}
+    char *getEtherPayloadStr(void) {
+          return _etherPayload;
+    }
 
 
-	int getEtherPacket(char *pktData, int pktDataBuffLen) {
+    int getEtherPacket(char *pktData, int pktDataBuffLen) {
 #define ETH_HEADER_LEN    14
         int i = 0;
 
         if (pktDataBuffLen < ETH_HEADER_LEN) {
-		    return -1;
-		}
+            return -1;
+        }
 
-		for (i = 0; i < MAC_ADDR_LEN; i++) {
+        for (i = 0; i < MAC_ADDR_LEN; i++) {
             pktData[i] = _dstMac[i];
         }
 
-		for (i = 0; i < MAC_ADDR_LEN; i++) {
+        for (i = 0; i < MAC_ADDR_LEN; i++) {
             pktData[MAC_ADDR_LEN + i] = _srcMac[i];
         }
 
         pktData[ETH_HEADER_LEN - 2] = ((_etherType >> 8) & 0xFF);
         pktData[ETH_HEADER_LEN - 1] = ((_etherType) & 0xFF);
 
-		int pktDataLen = convertHexPktStrToPkt(_etherPayload,
-				  							   &pktData[ETH_HEADER_LEN],
-											   pktDataBuffLen - ETH_HEADER_LEN);
-	    return (ETH_HEADER_LEN + pktDataLen);
-	}
+        int pktDataLen = convertHexPktStrToPkt(_etherPayload,
+                                               &pktData[ETH_HEADER_LEN],
+                                               pktDataBuffLen - ETH_HEADER_LEN);
+        return (ETH_HEADER_LEN + pktDataLen);
+    }
 
 private:
-	int  _srcMac[MAC_ADDR_LEN];
-	int  _dstMac[MAC_ADDR_LEN];
+    int  _srcMac[MAC_ADDR_LEN];
+    int  _dstMac[MAC_ADDR_LEN];
 
-	//
-	// ETH_P_IP =  0x0800
-	//
-	//
-	int  _etherType;
-	char _etherPayload[ETHER_PAYLOAD_BUF_SIZE];
+    //
+    // ETH_P_IP =  0x0800
+    //
+    //
+    int  _etherType;
+    char _etherPayload[ETHER_PAYLOAD_BUF_SIZE];
 };
 
 
@@ -135,16 +135,16 @@ public:
     //
     // Test Packed Id.
     //
-	typedef enum {
+    typedef enum {
         TestPackerIdInvalid = 0,
-		TEST_PKT_ID_IPV4_ECHO_REQ_TO_TAP1,
-		TEST_PKT_ID_IPV4_ECHO_REQ_TO_TAP2,
-		TEST_PKT_ID_IPV4_ROUTER_ICMP_ECHO_TO_TAP3,
+        TEST_PKT_ID_IPV4_ECHO_REQ_TO_TAP1,
+        TEST_PKT_ID_IPV4_ECHO_REQ_TO_TAP2,
+        TEST_PKT_ID_IPV4_ROUTER_ICMP_ECHO_TO_TAP3,
         TEST_PKT_ID_PUNT_ICMP_ECHO,
-		TEST_PKT_ID_IPV4_VLAN,
-		TEST_PKT_ID_MPLS_L2VLAN,
+        TEST_PKT_ID_IPV4_VLAN,
+        TEST_PKT_ID_MPLS_L2VLAN,
         TestPackerIdMax,
-	} TestPacketId;
+    } TestPacketId;
 
 
 
@@ -159,7 +159,7 @@ public:
     //
     //
     //
-	TestPacket* getTestPacket(TestPacketId testPktNum) {
+    TestPacket* getTestPacket(TestPacketId testPktNum) {
         return _testPacketLibrary[testPktNum];
 
     }
